30a80c47423497c0f1fb87b506ee78f58424a84b
deneme1 <a239514@drdrb.com>
2013-09-05 21:51:48 +0300

    test

---
+++ T4ViewMvc/Controllers/DummyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T4ViewMvc.Controllers
+{
+    public class DummyController : Controller
+    {
+        //
+        // GET: /Dummy/
+
+        public ActionResult Index(){
+            return View();
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+
+        public ActionResult Custom() {
+            return View();
+        }
+
+    }
+}
---
+++ T4ViewMvc/Models/Dummy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T4ViewMvc.Models
+{
+    public class Dummy
+    {
+        public int DummyId { get; set; }
+        public string Name { get; set; }
+        public bool IsExtraDummy { get; set; }
+    }
+}
